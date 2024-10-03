76dabf7ec719970ac84a6727e8ec900bd26c1055
Tarcisio Junior <tarcisiotj3@gmail.com>
2024-10-03 08:22:42 -0300

    primeira alteracao

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -48,34 +48,7 @@ async function getUserData(userName){
     screen.renderUser(user)
     
 
-    // getUser(userName).then(userData => {
-    //     let userInfo = `<div class="info">
-    //                         <img src="${userData.avatar_url}" alt="Foto do perfil do usuário" /> 
-    //                         <div class="data">
-    //                         <h1>${userData.name ?? 'Não possui nome cadastrado 😭' }</h1>
-    //                         <p>${userData.bio ?? 'Não possui bio cadastrada 😭'}</p>
-    //                         </div>
-    //                     </div>`
-
-    //     document.querySelector('.profile-data').innerHTML = userInfo
-
-    //     getUserRepositories(userName)
-    // })
-}
+  }
 
 getEvents()
 
-// function getUserRepositories(userName) {
-//     getRepositories(userName).then(reposData => {
-//         let repositoriesItens = ""
-//         reposData.forEach(repo => {
-//             repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`
-//         })
-
-//         document.querySelector('.profile-data').innerHTML += `<div> class="repositories section">
-//                                                                 <h2>Repositórios</h2>
-//                                                                 <ul>${repositoriesItens}</ul>
-//                                                               </div>`                                                            
-//     })
-
-// }
